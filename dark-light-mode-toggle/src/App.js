65e9ed955df5817c05dbacb8bd0920bb8dd6a522
Masoud RAVANDOUST <57909214+MRavandoust@users.noreply.github.com>
2022-07-15 10:35:00 +0200

    Blank Project

--- dark-light-mode-toggle/src/App.js
+++ dark-light-mode-toggle/src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Header from "./components/header/Header";
+import Hero from "./components/hero/Hero";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div>
-      <h1>Hello World !!!</h1>
+      <Header />
+      <Hero />
+      <Footer />
     </div>
   );
 }
